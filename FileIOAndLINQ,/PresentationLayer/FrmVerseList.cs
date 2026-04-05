d780b125b1adfe1749fff07fd7b70bd35bcb6999
Etendo <etendo123@gmail.com>
2026-04-04 22:25:48 -0600

    Add project files.

---
+++ FileIOAndLINQ,/PresentationLayer/FrmVerseList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileIOAndLINQ_.PresentationLayer
+{
+    public partial class FrmVerseList : Form
+    {
+        public FrmVerseList()
+        {
+            InitializeComponent();
+        }
+    }
+}
