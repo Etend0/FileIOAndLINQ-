218b66c7704e29e1a9ccabb5049c0fc3dcd21884
Etendo <etendo123@gmail.com>
2026-04-05 17:07:45 -0600

    Added data validation event handlers for the book and chapter of new verses.

--- FileIOAndLINQ,/PresentationLayer/FrmVerseList.cs
+++ FileIOAndLINQ,/PresentationLayer/FrmVerseList.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 /*
@@ -20,6 +21,9 @@ namespace FileIOAndLINQ.PresentationLayer
     {
         // Declare class level variables
         private List<Label> _errorLabels;
+        // Flags for user input
+        bool isValidBook = false, isValidChapter = false, isValidVerse = false;
+        bool isValidText = false, isValidMeaning = false, isValidImportance = false;
 
         /// <summary>
         /// Default constructor for FrmVerseList
@@ -52,6 +56,9 @@ namespace FileIOAndLINQ.PresentationLayer
             }
         }
 
+        /// <summary>
+        /// Set up the verse book combo box
+        /// </summary>
         private void InitializeBooks()
         {
             // Set up a list of books of the Bible
@@ -84,6 +91,66 @@ namespace FileIOAndLINQ.PresentationLayer
             cmbVerseBook.AutoCompleteMode = AutoCompleteMode.Suggest;
             // Set the autoCompleteSource to the list items
             cmbVerseBook.AutoCompleteSource = AutoCompleteSource.ListItems;
+        }
+
+        /// <summary>
+        /// Leave event handler for the book combo box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CmbVerseBookLeaveEH(object sender, EventArgs e)
+        {
+            // Check if the user has selected a book
+            if (cmbVerseBook.SelectedIndex >= 0)
+            {
+                // Set the book flag to true
+                isValidBook = true;
+                // Hide the book error label
+                lblBookError.Visible = false;
+            }
+            else
+            {
+                // Set the book flag to false
+                isValidBook = false;
+                // Update the book error label
+                lblBookError.Text = "You must select a book";
+                // Show the book error label
+                lblBookError.Visible = true;
+            }
+        }
+
+        /// <summary>
+        /// Leave event handler to make sure the user enters a number
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TxtVerseChapterLeaveEH(object sender, EventArgs e)
+        {
+            // Declare and Initialize
+            // ReGex pattern to check that the chapter is a number
+            Regex regex = new Regex(@"^[0-9]+$");
+            // Match object to hold the result of the ReGex comparison
+            Match match;
+
+            // Compare the regex pattern to the textbox text
+            match = regex.Match(txtVerseChapter.Text);
+            // Check if the match was a success
+            if (match.Success)
+            {
+                // Set the chapter flag to true
+                isValidChapter = true;
+                // Hide the chapter error label
+                lblChapterError.Visible = false;
+            }
+            else
+            {
+                // Set the chapter flag to false
+                isValidChapter = false;
+                // Update the chapter error label
+                lblChapterError.Text = "You must enter a number";
+                // Show the chapter error label
+                lblChapterError.Visible = true;
+            }
         }
     }
 }
